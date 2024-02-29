ae22a29cf1ced61d971dfa70aa53ad483b49448a
Gleb Tsiushkevich <76591527+uzlezz-soft@users.noreply.github.com>
2024-02-29 10:12:52 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,10 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	warnings "off"
+
+	targetdir (bin_dir .. "third-party/%{prj.name}")
+	objdir (obj_dir .. "third-party/%{prj.name}")
 
 	files
 	{
@@ -11,19 +15,37 @@ project "GLFW"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+	}
+
+	includedirs
+	{
+		"include"
 	}
 
 	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		
 		files
 		{
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
+			"src/posix_module.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
+			"src/posix_module.c",
 			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
@@ -35,11 +57,36 @@ project "GLFW"
 			"_GLFW_X11"
 		}
 
+	filter "system:macosx"
+		pic "On"
+
+		files
+		{
+			"src/cocoa_init.m",
+			"src/cocoa_monitor.m",
+			"src/cocoa_window.m",
+			"src/cocoa_joystick.m",
+			"src/cocoa_time.c",
+			"src/nsgl_context.m",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/osmesa_context.c",
+			"src/egl_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_COCOA"
+		}
+
 	filter "system:windows"
+		systemversion "latest"
+
 		files
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
